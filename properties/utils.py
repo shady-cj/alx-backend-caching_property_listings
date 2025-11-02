def8ea0a253326a377d57c30ff00254e650fab9a
shady-cj <petersp2000@gmail.com>
2025-11-02 22:11:07 +0100

    determining the hit ratios, keyspace hits and misses

--- properties/utils.py
+++ properties/utils.py
@@ -1,6 +1,11 @@
 from django.core.cache import cache
 from .models import Property
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 
 def get_all_properties():
     queryset = cache.get('all_properties')
@@ -10,3 +15,27 @@ def get_all_properties():
     
     return queryset
         
+
+def get_redis_cache_metrics():
+    from django_redis import get_redis_connection
+    conn = get_redis_connection("default")
+    info = conn.info()
+
+    hits = info.get("keyspace_hits", 0)
+    misses = info.get("keyspace_misses", 0)
+
+
+    if hits + misses == 0:
+        hit_ratio = None  # avoid division by zero
+    else:
+        hit_ratio = hits / (hits + misses)
+
+    logger.info(
+            f"Redis cache stats: hits={hits}, misses={misses}, hit_ratio={hit_ratio:.2%}"
+        )
+    metrics = {
+        "keyspace_hits": hits,
+        "keyspace_misses": misses,
+        "hit_ratio": hit_ratio,
+    }
+    return metrics
